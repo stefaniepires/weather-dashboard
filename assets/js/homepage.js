26eeb5add5f967d3accd827349b72e292eda5e57
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-18 20:46:47 -0600

    created form and added functionality to form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,7 +8,24 @@ var getUserCity = function(city, state) {
 }
 
 
-getUserCity("Dallas, TX");
 
 
+//var for the form: <form> and <input> elements
+var userFormEl = document.querySelector("#user-form");
+var cityInputEl = document.querySelector("#city");
 
+//to be executed upon a form submission browser event 
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  //get value from input element
+  var city = cityInputEl.value.trim();
+  if(city) {
+    getUserCity(city); //if there is a value, the value is passed to getUserCity function as argument 
+    cityInputEl.value=""; //clears form
+  } else {
+    alert("Please enter a city and state");
+  }
+};
+
+//event listener to userFormEl
+userFormEl.addEventListener("submit", formSubmitHandler);
