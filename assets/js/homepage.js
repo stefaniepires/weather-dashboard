2b3e545a42d2a08fb7c0830ed289ea39f8585d00
steftinsley <69828366+steftinsley@users.noreply.github.com>
2020-12-11 22:03:45 -0600

    added styling with bootstrap

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,7 @@
 var getUserCity = function(city, state) {
-  fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + "&units=imperial&appid=dfda35368bbd4b6f36f9ed7f2ede83c5").then( function (response) {
+  fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "," + state + "&cnt=5&units=imperial&appid=6a22242f54371d060e263ed6f93748a9").then( function (response) {
     response.json().then(function(data) {
+      //displayWeather(data, city);
       console.log(data);
       });
   });
@@ -13,6 +14,8 @@ var getUserCity = function(city, state) {
 //var for the form: <form> and <input> elements
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
+var weatherContainerEl = document.querySelector("#weather-container");
+var weatherSearchTerm = document.querySelector("#weather-search-term");
 
 //to be executed upon a form submission browser event 
 var formSubmitHandler = function(event) {
@@ -26,6 +29,19 @@ var formSubmitHandler = function(event) {
     alert("Please enter a city and state");
   }
 };
+
+/* var displayWeather = function(weather, searchTerm) {
+  if (weather.length === 0) {
+    weatherContainerEl.textContent = "No weather found.";
+    return;
+  }
+
+ // clear old content
+weatherContainerEl.textContent = "";
+weatherSearchTerm.textContent = searchTerm;
+};
+
+*/
 
 //event listener to userFormEl
 userFormEl.addEventListener("submit", formSubmitHandler);
